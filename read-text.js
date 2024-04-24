280ef40b6c5a8f0c0fd3e8f05690ca140e988354
Jaylen Goddard <113991585+jigodda@users.noreply.github.com>
2024-04-23 20:01:54 -0400

    Add files via upload

--- read-text.js
+++ read-text.js
@@ -1,6 +1,11 @@
 // Import Tesseract.js library
 const Tesseract = require('tesseract.js');
 
+const fs = require('fs');
+
+// File path where you want to save the text file
+const filePath = 'textFile.txt';
+
 // Function to extract text from an image
 function extractTextFromImage(imagePath) {
   // Use Tesseract.js to recognize text from the image
@@ -11,6 +16,7 @@ function extractTextFromImage(imagePath) {
   ).then(({ data: { text } }) => {
     console.log('Extracted Text:');
     console.log(text); // Print the extracted text to the console
+    writeText(text);
   }).catch(error => {
     console.error('Error during text extraction:', error);
   });
@@ -19,3 +25,15 @@ function extractTextFromImage(imagePath) {
 
 // Export the function to make it accessible in other files
 module.exports = { extractTextFromImage };
+
+
+function writeText(text){
+  // Write text to the file
+  fs.writeFile(filePath, text, (err) => {
+    if (err) {
+      console.error('Error saving the file:', err);
+      return;
+    }
+    console.log('Text saved to textFile.txt');
+  });
+}
